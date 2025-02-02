b0b1e438062fba1e249253bf9fab60e688f0f80d
HarshaTechKnight <sriharsha0413@gmail.com>
2025-02-02 12:34:30 +0530

    Employee-Management-Upload

---
+++ src/main/java/com/example/employee/Empl0yeeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Empl0yeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Empl0yeeManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/employee/Empl0yeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Empl0yeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
